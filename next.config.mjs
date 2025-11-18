7e69757f2e716b2e8197a00065230043be1cd98c
Damir Ospanbek <damir.ospanbek@maxinum.co>
2025-11-18 23:10:38 +0500

    fix: pomodoro config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  // Если репозиторий не в корне GitHub Pages, раскомментируйте следующую строку
+  // basePath: '/pomodoro',
+  // trailingSlash: true,
 }
 
 export default nextConfig
